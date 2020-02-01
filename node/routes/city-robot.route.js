be87d6fd991ae6446d35b28135f8de53f9f37591
Chreem <549078191@qq.com>
2020-02-01 09:20:09 +0800

    fix: 微信机器人返回格式

--- node/routes/city-robot.route.js
+++ node/routes/city-robot.route.js
@@ -1,15 +1,20 @@
 const { get } = require('../service/redis');
 
+const wxRobotResponse = (code = 200, data = {}) => ({
+    err_code: code,
+    data_list: [data]
+});
+
 module.exports = async (req, res) => {
     try {
         const city = req.query.city;
-        if (!city) return res.sendStatus(404);
+        if (!city) return res.send(wxRobotResponse(404));
         const areaData = JSON.parse(await get('AreaData')) || [];
         const result = areaData.filter(({ provinceName }) => {
             if (provinceName.indexOf(city) >= 0) return true;
         });
         const data = result[0];
-        if (!data) return res.sendStatus(404);
-        res.send(data);
-    } catch (e) { res.sendStatus(500) }
+        if (!data) return res.send(wxRobotResponse(404));
+        res.send(wxRobotResponse(200, data));
+    } catch (e) { res.send(wxRobotResponse(500)) }
 }
